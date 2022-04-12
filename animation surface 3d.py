5267fc4de6aeb0c33d44c1b6225c584908c4362f
Louis-Bernard St-Cyr <lbstc@ulaval.ca>
2022-04-11 21:48:23 -0400

    deuxieme commit

--- animation surface 3d.py
+++ animation surface 3d.py
@@ -11,6 +11,20 @@ X, Y = np.meshgrid(x_, y_)
 
 t_ = np.linspace(0, 2*pi, 90)
 
+x = np.linspace(1,100,50)
+y = np.linspace(1,100,50)
+t = np.linspace(1,5,100)
+X1, Y1 = np.meshgrid(x,y)
+
+
+a = 10
+sigma = 1
+def u(x,y,t):
+    alpha = 90-np.arctan(y/x)
+    b = 3
+    omega = (3*(a**2)*(b**2)-a**4)/(4*a)
+    res = 0.5*(a**2)*(1/np.cosh(0.5*a*(x-b*y-(omega*t/a))))**2
+    return res
 
 
 def f(x, y):
@@ -22,22 +36,42 @@ def g(x, y, t):
 fig = plt.figure()
 ax = fig.add_subplot(projection = "3d")
 
-def animate(n):
+# def animate(n):
+#     ax.cla()
+#     Z = g(X, Y, t_[n])
+#     colorfunction = (X**2+Y**2+Z**2)
+#     norm = mcolors.Normalize(colorfunction.min(), colorfunction.max())
+#     ax.plot_surface(
+#       X, Y, Z, rstride = 1, cstride = 1, facecolors=cm.jet(norm(colorfunction))
+#     )
+#     ax.set_zlim(0, 100)
+#     return fig
+
+def animate1(n):
     ax.cla()
-    Z = g(X, Y, t_[n])
-    colorfunction = (X**2+Y**2+Z**2)
+    Z = u(X1, Y1, t[n])
+    colorfunction = (X1**2+Y1**2+Z**2)
     norm = mcolors.Normalize(colorfunction.min(), colorfunction.max())
     ax.plot_surface(
-      X, Y, Z, rstride = 1, cstride = 1, facecolors=cm.jet(norm(colorfunction))
+      X1, Y1, Z, rstride = 1, cstride = 1, facecolors=cm.jet(norm(colorfunction))
     )
-    ax.set_zlim(0, 100)
+    ax.set_zlim(0, 50)
     return fig
 
 
-anim = FuncAnimation(
-  fig = fig, func = animate, frames = len(t_), interval = 1, repeat = False
+anim1 = FuncAnimation(
+  fig = fig, func = animate1, frames = len(t), interval = 1, repeat = True
 )
 
 writergif = PillowWriter(fps=30)
-anim.save('filename22.gif',writer=writergif)
+anim1.save('test.gif',writer=writergif)
+
+
+
+# anim = FuncAnimation(
+#   fig = fig, func = animate, frames = len(t_), interval = 1, repeat = False
+# )
+
+# writergif = PillowWriter(fps=30)
+# anim.save('filename232432.gif',writer=writergif)
 
